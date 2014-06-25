05c984288fa5c26eabcc427e245bc3bd642d54c6
moroz <morozarturv@gmail.com>
2014-06-25 15:49:16 +0300

    upd js

--- js/common.js
+++ js/common.js
@@ -1,69 +1,65 @@
 head.ready(function() {
 
-// info
-
+	// info
 	function info(){
-		var item = $(".js-info");
-		$('.js-info-a').hide();
-		item.each(function(){
-			var q = $(this).find('.js-info-q');
-			q.on('click', function(){
-				$(this).next('.js-info-a').slideToggle('fast');
-				return false;
+		var item = $('.js-info');
+		if (item.length) {
+			$('.js-info-a').hide();
+			item.each(function(){
+				var q = $(this).find('.js-info-q');
+				q.on('click', function(){
+					$(this).next('.js-info-a').slideToggle('fast');
+					return false;
+				});
 			});
-		});
+		};
 	}
-	if ($(".info").length) {
-		info();
-	};	
+	info();
 
-// reset search input
-
-    function reset_search() {
-        var input = $(".js-search");
-        $(".js-reset").hide();
-        input.keyup(function(){
-            if ( this.value !== 0 ) {
-                $(this).parent().find('.js-reset').show();
-            };
-        });
-        $('.js-reset').on('click', function(){
-            $(this).hide();
-        });
-
-    } 
-    if ($('.js-search').length) {
-    	reset_search();
+	// reset search input
+  function reset_search() {
+    var input = $('.js-search'),
+    		reset = $('.js-reset');
+    if (input.length) {
+    	reset.hide();
+    	input.keyup(function(){
+    	  if ( this.value !== 0 ) {
+    	    $(this).parent().find('.js-reset').show();
+    	  };
+    	});
+    	reset.on('click', function(){
+    	  $(this).hide();
+    	});
     };
+  } 
+  reset_search();
 
-// scroll top
-
-	$(".js-up").on('click', function(){
+	// scroll top
+	$('.js-up').on('click', function(){
 		$('html, body').animate({
-    	    scrollTop: $(".out").offset().top
-    	}, 500);
-    	return false;
+      scrollTop: $('.out').offset().top
+    }, 500);
+    return false;
 	});	 
 
-// banners slider
+	// banners slider
+	var slick_el = $('.js-banners');
+	if (slick_el.length) {
+		slick_el.slick({
+			infinite: true,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			autoplay: true,
+			dots: true
+		});
+	};
 
-	$('.js-banners').slick({
-		infinite: true,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		autoplay: true,
-		dots: true
-	});
-
-
-
-// document click
-
+	// document click
 	var agent = navigator.userAgent,
-	event = (agent.match(/iPad/i)) ? "touchstart" : "click";
+	event = (agent.match(/iPad/i)) ? 'touchstart' : 'click';
 
 	$(document).bind(event, function(e){
-		$(".js-popup").hide();
+		$('.js-popup').hide();
 	});
 
 });
