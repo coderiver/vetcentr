ac2c3f5df58d4dfe8e8e16ef4f5c4492b761d960
moroz <morozarturv@gmail.com>
2014-06-26 14:11:53 +0300

    upd

--- js/common.js
+++ js/common.js
@@ -76,4 +76,60 @@ head.ready(function() {
 		$('.js-popup').hide();
 	});
 
+	// tabs
+	var tabs_el = $('.js-tabs');
+	tabs_el.each(function(){
+		var tabs_btn = $(this).find('> .tabs__nav a'),
+			tabs_container = $(this).find('> .tabs__container'),
+			tabs_item = tabs_container.find('> .tabs__item');
+		tabs_item.hide();
+		tabs_item.first().show();
+		tabs_btn.on('click', function() {
+			if (!$(this).hasClass('is-active')) {
+				var id = $(this).attr('href');
+				tabs_btn.removeClass('is-active');
+				$(this).addClass('is-active');
+				tabs_item.hide();
+				$(id).fadeIn();
+				return false;
+			};
+		});
+	});
+
+	// services
+	var services_el = $('.js-services'),
+			services_el_item = services_el.find('.services__item'),
+			services_el_list = services_el.find('.services__list'),
+			services_el_title = services_el.find('.services__title'),
+			services_el_all = services_el.find('.services__all');
+	services_el_title.on('click', function(){
+		if (!$(this).parent().hasClass('is-active')) {
+			services_el_item.removeClass('is-active');
+			services_el_list.slideUp();
+			$(this).parent().addClass('is-active');
+			$(this).next().slideDown();
+		}
+		else {
+			$(this).parent().removeClass('is-active');
+			$(this).next().slideUp();
+		}
+	});
+	services_el_all.on('click', function(){
+		var text_show = $(this).data('show'),
+				text_hide = $(this).data('hide');
+		if (!$(this).hasClass('is-active')) {
+			$(this).addClass('is-active');
+			$(this).html(text_hide);
+			$(this).parent().find('.services__item').addClass('is-active');
+			$(this).parent().find('.services__list').slideDown();
+		}
+		else {
+			$(this).removeClass('is-active');
+			$(this).html(text_show);
+			$(this).parent().find('.services__item').removeClass('is-active');
+			$(this).parent().find('.services__list').slideUp();
+		};
+		return false;
+	});
+
 });
